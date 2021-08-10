32f496d3de7e291a514b9a4523ef6dd091880c1b
Luke Boelling <lboel@users.noreply.github.com>
2021-08-10 11:49:53 +0200

    style complete app code (fix list() Error in R 4.X

--- app.R
+++ app.R
@@ -7,31 +7,30 @@ library(ggplot2)
 library(shinyjs)
 library(shinydashboard)
 library(shinycssloaders)
-#
+
 
 # library(rsconnect)
 # deployApp()
-
-# Define UI for application that draws a histogram
 
 
 ui <- dashboardPage(
   # Application title
   dashboardHeader(title = "pavloviaShinyApp"),
   dashboardSidebar(
-    column(12,align = "center",offset = 0,
-           tags$style(".skin-blue .sidebar a { color: #444; }"),
-           
-           textInput("password", label = h3("Password"), placeholder = "Enter Password to get access to Data..."),
-           
-           # Button
-           conditionalPanel(
-             condition = "input.password == 'example'",
-             downloadButton("downloadData", "Download", ) %>% withSpinner(color = "#0dc5c1")
-           ))
+    column(12,
+      align = "center", offset = 0,
+      tags$style(".skin-blue .sidebar a { color: #444; }"),
+      textInput("password", label = h3("Password"), placeholder = "Enter Password to get access to Data..."),
+
+      # Button
+      conditionalPanel(
+        condition = "input.password == 'example'",
+        downloadButton("downloadData", "Download", ) %>% withSpinner(color = "#0dc5c1")
+      )
+    )
   ),
-  
-  
+
+
   # Main panel for displaying outputs ----
   dashboardBody(
     useShinyjs(),
@@ -39,7 +38,6 @@ ui <- dashboardPage(
       condition = "input.password == 'example'",
       DT::dataTableOutput("dataOverview")
     )),
-    
     fluidRow(conditionalPanel(
       condition = "input.password == 'example'",
       plotOutput("plotTest")
@@ -48,50 +46,47 @@ ui <- dashboardPage(
 )
 
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
-  
-  
-  # Reactive value for selected dataset ----
   datasetInput <- reactiveVal({
     disable("downloadData")
     token <- read_file("token") # Personal Access Token for the Project
-    project_id <- 104201 # Project ID
+    project_id <- 149 # Project ID
     gitlabPavloviaURL <- paste0("https://gitlab.pavlovia.org/api/v4/projects/", project_id, "/repository/archive.zip") # API - URL to download whole repository
     r <- GET(gitlabPavloviaURL, add_headers("PRIVATE-TOKEN" = token)) # Getting Archive
-    
+
     bin <- content(r, "raw") # Writing Binary
-    
+
     temp <- tempfile() # Init Tempfile
-    
+
     writeBin(bin, temp) # Write Binary of Archive to Tempfile
-    
+
     listofFiles <- unzip(
       zipfile = temp, overwrite = T,
       junkpaths = T, list = T
     ) # Unzip only list of all files in the archive.zip file
-    
-    
+
+
     csvFiles <- grep("*.csv", x = listofFiles$Name, value = T) # Grep only the csv Files (Pattern can be extended to get only data-csv file)
-    
+
     unzip(
       zipfile = temp, overwrite = T,
-      junkpaths = T, files = csvFiles, exdir = "temp"
+      junkpaths = T, files = csvFiles[1:100], exdir = "temp"
     ) # Unzip the csv Files in the temp-file
-    
+
     csvFilesPaths <- list.files("temp/", full.names = T) # Get the unzipped csv-Files in the temp-directory
-    
-    
+
+
     # To get only Valid CSV-Files and enable us to filter by DateTime of the File we can parse the files standard date-time string in the Pavlovia-Default FileNames
     dateTimeOfFiles <- tibble(filepaths = csvFilesPaths) %>%
       mutate(dateTime = str_extract(filepaths, "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}h[0-9]{2}")) %>%
       filter(!is.na(dateTime)) %>%
       mutate(dateTime = parse_datetime(dateTime, "%Y-%m-%d_%Hh%M"))
     # %>%  filter(dateTime > parse_datetime("2019-02-01_15h00", "%Y-%m-%d_%Hh%M")) # This can be used to Filter by a specific time
-    
-    
+
+
     # Purrr Magic  - Thanks to https://clauswilke.com/blog/2016/06/13/reading-and-combining-many-tidy-data-files-in-r/
-    
+
     # Now the read the desired data Files with purrr:
     data <- data_frame(filename = dateTimeOfFiles$filepaths) %>% # create a data frame
       # holding the file names
@@ -101,32 +96,31 @@ server <- function(input, output) {
           ~ read_csv(file.path(.))
         ) # a new data column
       )
-    
+
     # Unlink temp because we don't need it anymore
-    unlink("temp/*")
+    unlink("temp", recursive = T)
     disable("downloadData")
     data
   })
-  
-  
+
+
 
   output$plotTest <- renderPlot({
-    ggplot(dataMerged(), aes(y=resp.rt, x = congruent), color=participant) +
-     stat_summary(geom="point",fun = "mean") + 
-     stat_summary(geom="errorbar", fun.data = mean_se)
+    ggplot(dataMerged(), aes(y = resp.rt, x = congruent), color = participant) +
+      stat_summary(geom = "point", fun = "mean") +
+      stat_summary(geom = "errorbar", fun.data = mean_se)
   })
-  
+
   # Table of selected dataset ----
   output$dataOverview <- DT::renderDataTable({
-    
-    DT::datatable(  datasetInput() %>%
-                      rowwise() %>%
-                      mutate(participant = file_contents$participant[1], fileDim = paste0("Rows:", dim(file_contents)[1], " Vars:", dim(file_contents)[2])[1]) %>%
-                      select(-file_contents)
-                    , options = list(scrollX = TRUE))
-    
+    DT::datatable(datasetInput() %>%
+      rowwise() %>%
+      mutate(participant = list(file_contents$participant[1]), fileDim = paste0("Rows:", dim(file_contents)[1], " Vars:", dim(file_contents)[2])[1]) %>%
+      select(-file_contents),
+    options = list(scrollX = TRUE)
+    )
   })
-  
+
   observeEvent(input$password, {
     if (input$password != "example") hide("table") else show("table")
   })
@@ -134,14 +128,13 @@ server <- function(input, output) {
     req(datasetInput())
     if (input$password != "example") disable("downloadData") else enable("downloadData")
   })
-  
+
   dataMerged <- reactive({
     # Read in all available data in a single tibble
     datasetInput() %>% select(file_contents) %>% # remove filenames, not needed anynmore
       unnest(cols = c(file_contents))
-    
   })
-  
+
   output$downloadData <- downloadHandler(
     filename = function() {
       paste("Test_Data", ".csv", sep = "")
